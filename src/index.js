148f0ecaad7317a13ad0a1f51bf9416a19234c30
John Lim (Win Desktop) <limjohn@uw.edu>
2019-10-12 09:11:12 -0800

    simple interaction

--- src/index.js
+++ src/index.js
@@ -4,14 +4,47 @@ import './scss/main.scss';
 import $ from 'jquery';
 import anime from 'animejs/lib/anime.es.js';
 
+const _body = $("body");
+const _logo = $(".logo");
+
 $(function() {
     // start up your app
-    anime({
+    let animation = anime({
         targets: '.logo',
         opacity: [0, 1],
         translateY: ["20%", "0"],
         delay: 500,
         duration: 1000,
-        easing: 'easeOutCubic'
+        easing: 'easeOutCubic',
+        complete: function(anim) {
+            animation = null;
+        }
     });
+
+    // Events interact
+    _logo.on("mousedown touchstart", function() {
+        if (animation) {
+            animation.seek(animation.duration);
+        }
+        anime.remove(".logo");
+        anime({
+            targets: '.logo',
+            scale: 1.2,
+            duration: 600
+        });
+    });
+
+    _logo.on("mouseup touchend", function() {
+        if (animation) {
+            animation.seek(animation.duration);
+        }
+        anime.remove(".logo");
+        anime({
+            targets: '.logo',
+            scale: 1,
+            duration: 200,
+            easing: 'easeOutCubic'
+        });
+    });
+
 });
